ce2eccc98ebeaa6961c1af20aee6fd807e2304ad
SaqerPunch <115329139+SaqerPunch@users.noreply.github.com>
2023-10-05 14:12:25 -0400

    Update 1 Chess.java
    Finished Start class

--- Chess.java
+++ Chess.java
@@ -2,14 +2,17 @@ package chess;
 
 import java.util.ArrayList;
 
+import chess.ReturnPiece.PieceFile;
+import chess.ReturnPiece.PieceType;
+
 class ReturnPiece {
 	static enum PieceType {WP, WR, WN, WB, WQ, WK, 
 		            BP, BR, BN, BB, BK, BQ};
 	static enum PieceFile {a, b, c, d, e, f, g, h};
 	
 	PieceType pieceType;
-	PieceFile pieceFile;
-	int pieceRank;  // 1..8
+	PieceFile pieceFile; //Letter Part of Coordinate
+	int pieceRank;  // 1..8. Number Part of Coordinate
 	public String toString() {
 		return ""+pieceFile+pieceRank+":"+pieceType;
 	}
@@ -53,7 +56,12 @@ public class Chess {
 		
 		/* FOLLOWING LINE IS A PLACEHOLDER TO MAKE COMPILER HAPPY */
 		/* WHEN YOU FILL IN THIS METHOD, YOU NEED TO RETURN A ReturnPlay OBJECT */
-		return null;
+
+		ReturnPlay play = new ReturnPlay();
+
+		play.piecesOnBoard.
+
+		return play;
 	}
 	
 	
@@ -62,5 +70,116 @@ public class Chess {
 	 */
 	public static void start() {
 		/* FILL IN THIS METHOD */
+		ReturnPlay beginning = new ReturnPlay();
+		beginning.piecesOnBoard.clear();
+		beginning.message = null;
+
+		for(PieceFile x : PieceFile.values()){
+			
+			//adds white pawns to board
+			ReturnPiece pawnWhite = new ReturnPiece();
+			pawnWhite.pieceFile = x;
+			pawnWhite.pieceRank = 2;
+			pawnWhite.pieceType = PieceType.WP;
+			beginning.piecesOnBoard.add(pawnWhite);
+
+			// adds black pawns to board
+			ReturnPiece pawnBlack = new ReturnPiece();
+			pawnBlack.pieceFile = x;
+			pawnBlack.pieceRank = 7;
+			pawnBlack.pieceType = PieceType.BP;
+			beginning.piecesOnBoard.add(pawnBlack);
+		}
+		
+		//adds White Rook
+		ReturnPiece whiteRook = new ReturnPiece();
+		whiteRook.pieceFile = PieceFile.a;
+		whiteRook.pieceRank = 1;
+		whiteRook.pieceType = PieceType.WR;
+		beginning.piecesOnBoard.add(whiteRook);
+
+		whiteRook.pieceFile = PieceFile.h;
+		beginning.piecesOnBoard.add(whiteRook);
+
+		//adds Black Rook
+		ReturnPiece blackRook = new ReturnPiece();
+		blackRook.pieceFile = PieceFile.a;
+		blackRook.pieceRank = 8;
+		blackRook.pieceType = PieceType.BR;
+		beginning.piecesOnBoard.add(blackRook);
+
+		blackRook.pieceFile = PieceFile.h;
+		beginning.piecesOnBoard.add(blackRook);
+
+
+		//adds White Knight
+		ReturnPiece whiteKnight = new ReturnPiece();
+		whiteKnight.pieceFile = PieceFile.b;
+		whiteKnight.pieceRank = 1;
+		whiteKnight.pieceType = PieceType.WK;
+		beginning.piecesOnBoard.add(whiteKnight);
+
+		whiteKnight.pieceFile = PieceFile.g;
+		beginning.piecesOnBoard.add(whiteKnight);
+
+		//adds Black Knight
+		ReturnPiece BlackKnight = new ReturnPiece();
+		BlackKnight.pieceFile = PieceFile.b;
+		BlackKnight.pieceRank = 8;
+		BlackKnight.pieceType = PieceType.BK;
+		beginning.piecesOnBoard.add(BlackKnight);
+
+		BlackKnight.pieceFile = PieceFile.g;
+		beginning.piecesOnBoard.add(BlackKnight);
+
+
+		//adds White Bishop
+		ReturnPiece WhiteBishop = new ReturnPiece();
+		WhiteBishop.pieceFile = PieceFile.c;
+		WhiteBishop.pieceRank = 1;
+		WhiteBishop.pieceType = PieceType.WB;
+		beginning.piecesOnBoard.add(WhiteBishop);
+
+		WhiteBishop.pieceFile = PieceFile.f;
+		beginning.piecesOnBoard.add(WhiteBishop);
+
+		//adds Black Bishop
+		ReturnPiece BlackBishop = new ReturnPiece();
+		BlackBishop.pieceFile = PieceFile.c;
+		BlackBishop.pieceRank = 8;
+		BlackBishop.pieceType = PieceType.BB;
+		beginning.piecesOnBoard.add(BlackBishop);
+
+		BlackBishop.pieceFile = PieceFile.f;
+		beginning.piecesOnBoard.add(BlackBishop);
+
+		//adds White Queen
+		ReturnPiece WhiteQueen = new ReturnPiece();
+		WhiteQueen.pieceFile = PieceFile.d;
+		WhiteQueen.pieceRank = 1;
+		WhiteQueen.pieceType = PieceType.WQ;
+		beginning.piecesOnBoard.add(WhiteQueen);
+
+		//adds Black Queen
+		ReturnPiece BlackQueen = new ReturnPiece();
+		BlackQueen.pieceFile = PieceFile.d;
+		BlackQueen.pieceRank = 8;
+		BlackQueen.pieceType = PieceType.BQ;
+		beginning.piecesOnBoard.add(BlackQueen);
+
+		//adds White King
+		ReturnPiece WhiteKing = new ReturnPiece();
+		WhiteKing.pieceFile = PieceFile.e;
+		WhiteKing.pieceRank = 1;
+		WhiteKing.pieceType = PieceType.WK;
+		beginning.piecesOnBoard.add(WhiteKing);
+
+		//adds Black King
+		ReturnPiece BlacKKing = new ReturnPiece();
+		BlacKKing.pieceFile = PieceFile.e;
+		BlacKKing.pieceRank = 8;
+		BlacKKing.pieceType = PieceType.BK;
+		beginning.piecesOnBoard.add(BlacKKing);
+		
 	}
 }
