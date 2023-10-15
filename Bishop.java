1526b34a99cc2e3249210e145f4ecb285222c77a
tommyfeeley <73509256+tommyfeeley@users.noreply.github.com>
2023-10-15 02:59:35 -0400

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -2,7 +2,7 @@ package chess;
 
 public class Bishop extends Pieces{
     public Bishop(boolean isWhite){
-        super(isWhite);
+        super(isWhite); 
 
         if(isWhite == true){
             pieceType = PieceType.WB;
@@ -11,8 +11,57 @@ public class Bishop extends Pieces{
         }
         
     }
+    @Override 
+    public boolean canMove(Board board, String move){
+        // Check if desired move is on valid diagonal
+        char startFile = move.charAt(0);
+        int startRank = Character.getNumericValue(move.charAt(1));
+        char endFile = move.charAt(3);
+        int endRank = Character.getNumericValue(move.charAt(4));
 
-    public boolean canMove(ReturnPlay board, String move){
-        return true;
+        // Check if absolute value of ranks and files are equal 
+        int rankDifference = Math.abs(endRank - startRank);
+        int fileDifference = Math.abs(endFile - startFile);
+
+        if (rankDifference == fileDifference) {
+            int rankStep;
+            if (endRank > startRank) {
+                rankStep = 1;
+            } else {
+                rankStep = -1;
+            }
+        }   int fileStep;
+            if (endFile > startFile) {
+                fileStep = 1;
+            } else {
+                fileStep = -1;
+            }
+
+            char currentFile = (char) (startFile + fileStep);
+            int currentRank = startRank + rankStep;
+
+            while (currentFile != endFile && currentRank != endRank) {
+                if (board.isOccupied(currentFile, currentRank)) {
+                    // Below we're checking for pins
+                    if (board.isOccupied(currentFile, currentRank)) {
+                        Pieces pinningPiece = board.getPiece(currentFile, currentRank);
+                        if (pinningPiece.isWhite() != this.isWhite()) {
+                            return true;    // The bishop can capture the pinning piece
+                        } else {
+                            return false;  // The bishop is pinned and can NOT move
+                        }
+                    }
+                }
+                currentFile = (char) (currentFile + fileStep);
+                currentRank += rankStep;
+            }
+            
+            if (board.isOccupied(endFile, endRank)) {
+                if (board.getPiece(endFile, endRank).isWhite() == this.isWhite()) {
+                    return false;
+                }
+            }
+            return true;
     }
+    return false;
 }
