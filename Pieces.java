de6c362071db98148f1c435ac97a55a02cf132bf
tommyfeeley <73509256+tommyfeeley@users.noreply.github.com>
2023-10-14 23:52:25 -0400

    Update Pieces.java

--- Pieces.java
+++ Pieces.java
@@ -4,7 +4,43 @@ import java.util.*;
 public abstract class Pieces {
     public boolean isWhite;
     public boolean captured;
+    
+    public int pieceRank;
+    public ReturnPiece.PieceType pieceType;
+    public ReturnPiece.PieceFile pieceFile;
 
+
+    /**
+     * Commented out so it doesnt break stuff
+     * 
+     * public Pieces(ReturnPiece.PieceType pieceType, ReturnPiece.PieceFile pieceFile, int pieceRank) { 
+     *      this.pieceType = pieceType;
+     *      this.pieceFile = pieceFile;
+     *      this.pieceRank = pieceRank;
+     * }
+     * 
+    */ 
+
+    public getPieceRank() {
+        return pieceRank;
+    }
+
+    public void setPieceRank(int pieceRank) {
+        this.pieceRank = pieceRank;
+    }
+
+    public getPieceFile() {
+        return pieceFile;
+    }
+
+    public setPieceFile(ReturnPiece.PieceFile pieceFile) {
+        this.pieceFile = pieceFile;
+    }
+
+    public ReturnPiece.PieceType getPieceType() {
+        return pieceType;
+    }
+    
     public Pieces(boolean white) {
         this.setWhite(white);
     }
