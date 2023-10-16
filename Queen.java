ba142ce711648f45655c769ed37796bbebadd720
tommyfeeley <73509256+tommyfeeley@users.noreply.github.com>
2023-10-16 02:35:34 -0400

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -13,6 +13,92 @@ public class Queen extends Pieces{
     }
 
     public boolean canMove(ReturnPlay board, String move){
-        return true;
+        char startFile = move.charAt(0);
+        int startRank = Character.getNumericValue(move.charAt(1));
+        char endFile = move.charAt(3);
+        int endRank = Character.getNumericValue(move.charAt(4));
+
+        // CHeck if queen is trying to move to own square
+        if (startFile == endFile && startRank == endRank) {
+            return false;
+        }
+
+        int rankDifference = Math.abs(endRank - startRank); 
+        int fileDifference = Math.abs(endFile - startFile);
+
+        if (rankDifference == fileDifference) {
+            int rankStep = (endRank > startRank) ? 1 : -1;
+            int fileStep = (endFile > startFile) ? 1 : -1;
+
+            char currentFile = startFile;
+            int currentRank = startRank;
+
+            char nextSquareFile = (char) (startFile + fileStep);
+            int nextSquareRank = startRank + rankStep;
+
+            while (currentFile != endFile && currentRank != endRank) {
+                String currentMove = "" + currentFile + currentRank + " " + nextSquareFile + nextSquareRank;
+                if (isOccupied(board, currentMove)) {
+                    return false;
+                }
+                currentFile = (char) (currentFile + fileStep);
+                currentRank += rankStep;
+            }
+            if (currentFile == endFile && currentRank == endRank) {
+                String currentMove = "" + currentFile + currentRank + " " + nextSquareFile + nextSquareRank;
+                if (isOccupied(board, currentMove) == true) {
+                    if (spacePiece(board, currentMove).isWhite() == true && isWhite == true) {
+                        return false;
+                    }   else if (spacePiece(board, currentMove).isWhite == false && isWhite == false) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                } else {
+                    return true;
+                }
+            }
+            return false;
+        }
+        if (startFile == endFile || startRank == endRank) {
+            int fileStep = (startFile == endFile) ? 0 : (startFile < endFile) ? 1 : -1;
+            int rankStep = (startRank == endRank) ? 0 : (startRank < endRank) ? 1 : -1;
+
+            char currentFile = (char) (startFile + fileStep);
+            int currentRank = startRank + rankStep;
+
+            while (currentFile != endFile || currentRank != endRank) {
+                String currentMove = "" + currentFile + currentRank + " " + endFile + currentRank;
+                if (isOccupied(board, currentMove)) {
+                    return false;
+                }
+                currentFile = (char) (currentFile + fileStep);
+                currentRank += rankStep;
+            }
+
+            if (currentFile == endFile && currentRank == endRank) {
+                String currentMove = "" + currentFile + currentRank + " " + endFile + currentRank;
+                if (isOccupied(board, currentMove) == true) {
+                    if (spacePiece(board, currentMove).isWhite == true && isWhite == true) {
+                        return false;
+                    } else if (spacePiece(board, currentMove).isWhite == false && isWhite == false) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                } else {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // If the move is neither a bishop or a rook, invalid
+        return false;
     }
-}
+        
+
+    }
+
+
