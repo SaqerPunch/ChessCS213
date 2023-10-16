a60ff7882c3a8a7692be37b55680744e4ba4e30f
SaqerPunch <115329139+SaqerPunch@users.noreply.github.com>
2023-10-16 02:53:53 -0400

    Fixes Hopping

--- Rook.java
+++ Rook.java
@@ -27,12 +27,73 @@ public class Rook extends Pieces {
         if (startFile == endFile && startRank == endRank) { // 
             return false;
         }
-        
+
         if (startFile == endFile || startRank == endRank) {
+            int rankStep = 0;
+            int fileStep = 0;
+            
+            
+                
+            if (endRank > startRank) {
+                rankStep = 1;
+            }else if(endRank == startRank){
+                rankStep = 0;
+            }else {
+                rankStep = -1;
+            }
+
+            if (endFile > startFile) {
+                fileStep = 1;
+            }else if(endFile == startFile){
+                fileStep = 0;
+            }else {
+                fileStep = -1;
+            }
+            
+            System.out.println("Rank Step is " + rankStep);
+            System.out.println("File Step is " + fileStep);
+
+            char currentFile = startFile;
+            int currentRank = startRank;
+
+            char nextSquareFile = (char)(startFile + fileStep);
+            int nextSquareRank = startRank + rankStep;
+
+            while (currentFile != endFile || currentRank != endRank) {
+                String currentMove =""+currentFile+currentRank+" "+""+nextSquareFile+nextSquareRank;
+                //System.out.println(currentMove);
+                //System.out.println(isOccupied(board, currentMove));
+                if (isOccupied(board, currentMove)) {
+                    return false;  
+                }
+                currentFile = (char) (currentFile + fileStep);
+                currentRank += rankStep;
+
+                nextSquareFile = (char)(currentFile + fileStep);
+                nextSquareRank = currentRank + rankStep;
+
+            }
+                
+            if(currentFile == endFile && currentRank == endRank){
+                System.out.println("REACHES HERE");
+                String currentMove =""+currentFile+currentRank+" "+""+currentFile+currentRank;
+                System.out.println(currentMove);
+                if(isOccupied(board, currentMove)== true){
+                    if(spacePiece(board, currentMove).isWhite == true && isWhite == true){
+                        return false;
+                    }else if(spacePiece(board, currentMove).isWhite == false && isWhite == false){
+                        return false;
+                    }else{
+                        return true;
+                    }
+                }else{
+                    return true;
+                }
+            }
+        }
+           
             return true;
-        }
-        return false;
-        }
+        }    
     }
 
 
